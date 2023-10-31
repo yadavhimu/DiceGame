964d92d761c2f648b0ba5edb8e890f62540a2636
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2023-10-31 21:29:40 +0530

    img fix

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -5,7 +5,7 @@ import { Button } from "../styled/Button"
 const StartGame = ({ toggle }) => {
   return (
     <Container> 
-      <div ><img src='https://images.unsplash.com/photo-1568743966689-d37c04538535?auto=format&fit=crop&q=80&w=1932&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'></img></div>
+      <div ><img src='./images/dices 1.png'></img></div>
       <div className="content">
         <h1>
           Dice Game
